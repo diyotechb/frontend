d29cffe572da2105970a0def501de916139d4644
Sudip Adhikari <shudip79@gmail.com>
2023-12-13 14:07:51 -0500

    added validation

--- src/app/components/pages/job-application/job-application.component.ts
+++ src/app/components/pages/job-application/job-application.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AppService } from 'src/app/app.service';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-job-application',
@@ -10,39 +10,51 @@ import { NgForm } from '@angular/forms';
 })
 export class JobApplicationComponent implements OnInit {
 
-  firstName: string = '';
-  middleName: string = '';
-  lastName: string = '';
-  applicantEmail: string = '';
-  phoneNumber: string = '';
-  selectedFileName: string = '';
   message: string = '';
+  selectedFileName: string = '';
   selectedFile: File | null = null;
+  applicationForm: FormGroup;
 
-  constructor(private appService:AppService, private snackBar: MatSnackBar) {}
-  
-  ngOnInit(): void {
-   
+  constructor(private appService:AppService, private snackBar: MatSnackBar, private fb: FormBuilder) {
+    this.applicationForm = this.fb.group({
+      firstName: ['', Validators.required],
+      middleName: [''],
+      lastName: ['', Validators.required],
+      applicantEmail: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+      phoneNumber: ['', [Validators.required, Validators.pattern('^[0-9- ]+$')]],
+    });
   }
+  
+  ngOnInit(): void {}
 
   onFileSelected(event: any) {
     this.selectedFile = event.target.files[0];
     this.selectedFileName = this.selectedFile?.name || '';
   }
 
-  @ViewChild('applicationForm') applicationForm: NgForm;
-
   submitApplication() {
-    const formData = new FormData();
-    formData.append('firstName', this.firstName);
-    formData.append('middleName', this.middleName);
-    formData.append('lastName', this.lastName);
-    formData.append('email', this.applicantEmail);
-    formData.append('phoneNumber', this.phoneNumber);
-
-    if (this.selectedFile) {
-      formData.append('resume', this.selectedFile);
+    if(this.applicationForm.invalid){
+      const message = 'Fill all the required fields';
+          this.snackBar.open(message, 'Close', {
+            duration: 3000,
+          });
+      return;
     }
+
+    if(!this.selectedFile){
+      const message = 'Resume is required';
+          this.snackBar.open(message, 'Close', {
+            duration: 3000,
+          });
+      return;
+    }
+    const formData = new FormData();
+    formData.append('firstName', this.applicationForm.value.firstName);
+    formData.append('middleName', this.applicationForm.value.middleName);
+    formData.append('lastName', this.applicationForm.value.lastName);
+    formData.append('email', this.applicationForm.value.applicantEmail);
+    formData.append('phoneNumber', this.applicationForm.value.phoneNumber);
+    formData.append('resume', this.selectedFile);
 
     this.appService.applyJob(formData)
       .subscribe(
@@ -51,7 +63,10 @@ export class JobApplicationComponent implements OnInit {
           this.snackBar.open(message, 'Close', {
             duration: 3000,
           });
-          this.applicationForm.resetForm();
+          this.applicationForm.reset();
+          this.selectedFile = null;
+          this.selectedFileName = '';
+
           this.message = message;
         },
         (error) => {
