66169e33437020043c6b4961529357e1709a0a93
Bishwo Bhandari <bishwo@diyotech.net>
2023-12-18 08:50:20 -0500

    prod env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  prodURL : 'http://diyotech-demo-env.eba-ujvs9yyh.us-east-1.elasticbeanstalk.com',
+  prodURL : 'https://diyotech-demo-env.eba-ujvs9yyh.us-east-1.elasticbeanstalk.com',
 };
